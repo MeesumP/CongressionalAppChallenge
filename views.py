f1d8292f91acbb454b443fa77c95e36c7d2a015d
Panjwani <20262115@hxjk529f265d.home>
2023-09-23 16:01:10 -0400

    added hmlt docs, routed in backend

---
+++ views.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template
+
+views = Blueprint(__name__, 'views')
+
+@views.route('/')
+def home_page():
+    return render_template('home.html') #can pass variables to template that render with proper way to call in html code
+
+@views.route('/signup')
+def signup_page():
+    return render_template('signup.html')
+
+@views.route('/results')
+def results_page():
+    return render_template('signup.html')
