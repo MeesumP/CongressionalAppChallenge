d7f3d5b43e727c8f390713f31f9efe902c7fd70e
Panjwani <20262115@hxjk529f265d.home>
2023-09-23 15:57:58 -0400

    real setup again on proper branch

--- Website/views.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-views = Blueprint(__name__, 'views')
-
-@views.route('/')
-def home():
-    return render_template('home.html') #can pass variables to template that render with proper way to call in html code
